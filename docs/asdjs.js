7cab1dd760f2d06f31d199cab0d167119ccdb6b2
raychunghk <mannchung@gmail.com>
2021-12-28 21:43:52 +0800

    Update asdjs.js

--- docs/asdjs.js
+++ docs/asdjs.js
@@ -1,34 +1,45 @@
- 
 $(function() {
-     $('#btntheme2').on('click', function() {
+    $('#btntheme2').on('click', function() {
 
 
-        if ($('body').hasClass('asdstyle2')) {
-            $('body').removeClass('asdstyle2')
-            $(this).css('background', 'white')
-        } else {
-            $('body').addClass('asdstyle2')
-            $(this).css('background', 'gold')
-        }
+
     })
 
     $(document).keydown(function(event) {
         if (event.altKey && event.which === 49) {
-            switchstyle(2)
-            e.preventDefault();
+            switchstyle(1)
+            event.preventDefault();
         }
         if (event.altKey && event.which === 50) {
-            switchstyle(3)
-            e.preventDefault();
+            switchstyle(4)
+            event.preventDefault();
         }
-        if (event.altKey && event.which === 53) {
-            switchstyle(1)
-            e.preventDefault();
+        if (event.altKey && event.which === 51) {
+            switchstyle(2)
+            event.preventDefault();
+        }
+        if (event.altKey && event.which === 52) {
+            switchstyle(3)
+            event.preventDefault();
         }
     });
 })
 
+function removeAllStyle() {
+    $([1, 2, 3, 4, 5, 5]).each(function(i, v) {
+
+        stylename = 'asdstyle' + this
+        console.log(stylename)
+        if ($('body').hasClass(stylename)) {
+            console.log(this);
+            $('body').removeClass(stylename)
+
+        }
+    })
+}
+
 function switchstyle(styleno) {
+    removeAllStyle();
     stylename = 'asdstyle' + styleno
     if ($('body').hasClass(stylename)) {
         $('body').removeClass(stylename)
